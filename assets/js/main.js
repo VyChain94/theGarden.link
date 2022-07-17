a549bcc4e059ec28e20425e7bbf47bb3bd863bc6
VyChain94 <ivyetigsjr6@protonmail.com>
2022-07-17 18:19:39 -0400

    lilJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,29 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+document.getElementById("name").onchange = name
+document.getElementById("email").onchange = email
+document.getElementById("message").onchange = message
+document.getElementById("ourwork").onmouseover = ourwork
+
+function name(){
+	document.getElementById("name").style.background="green"
+	document.getElementById("name").style.color="white"
+	document.getElementById("name").style.fontWeight="400"
+}
+
+function email(){
+	document.getElementById("email").style.background="green"
+	document.getElementById("email").style.color="white"
+	document.getElementById("email").style.fontWeight="400"
+}
+
+function message(){
+	document.getElementById("message").style.background="green"
+	document.getElementById("message").style.color="white"
+	document.getElementById("message").style.fontWeight="400"
+}
+
 (function($) {
 
 	var	$window = $(window),
